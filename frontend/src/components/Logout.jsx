a78b2103794b8f2e9666c21f6bd0baad194b3bc8
“Kaustubh” <koustubhgade@gmail.com>
2023-07-21 10:19:32 +0530

    first commit

---
+++ frontend/src/components/Logout.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BiPowerOff } from "react-icons/bi";
+import styled from "styled-components";
+import axios from "axios";
+import { logoutRoute } from "../utils/APIRoutes";
+export default function Logout() {
+  const navigate = useNavigate();
+  const handleClick = async () => {
+    const id = await JSON.parse(
+      localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+    )._id;
+    const data = await axios.get(`${logoutRoute}/${id}`);
+    if (data.status === 200) {
+      localStorage.clear();
+      navigate("/login");
+    }
+  };
+  return (
+    <Button onClick={handleClick} title="Log out" className="addMore">
+      <BiPowerOff />
+    </Button>
+  );
+}
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: #9a86f3;
+  border: none;
+  cursor: pointer;
+  svg {
+    font-size: 1.3rem;
+    color: #ebe7ff;
+  }
+  .addMore {
+    border: none;
+    width: 32px;
+    height: 32px;
+    background-color: #eee;
+    transition: all ease-in-out 0.2s;
+    cursor: pointer;
+  }
+  .addMore:hover {
+    border: 1px solid #888;
+    background-color: #ddd;
+  }
+`;
